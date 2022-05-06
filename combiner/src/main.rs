7c47c1147486702f0859480f5486464128f1ed6c
Alessio Premoli <alessio.premoli@gmail.com>
2022-05-06 10:40:36 +0200

    fix

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,3 +1,17 @@
+mod args;
+use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat};
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::new();
+    println!("{:?}", args);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
 }
